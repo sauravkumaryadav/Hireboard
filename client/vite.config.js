7942a1c71b841d51dead1704236ff2567d358b22
Saurav Kumar Yadav <sauravskyjiit@gmail.com>
2026-04-16 17:44:58 +0530

    server file initial setup and mongo db connection

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host:'localhost',
     proxy: {
       '/api': 'http://localhost:5000',
     },
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-
+import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
@@ -10,4 +9,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
