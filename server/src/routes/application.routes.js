55fa1ec50fb9dbb1efac21d729f3a9868f3cba7e
Saurav Kumar Yadav <sauravskyjiit@gmail.com>
2026-04-23 11:20:58 +0530

    added protect middleware in application apis

--- server/src/routes/application.routes.js
+++ server/src/routes/application.routes.js
@@ -3,14 +3,14 @@
 import express from 'express';
 import Application from '../models/Application.js';
 import { createApplication, deleteApplication, getApplications, updateApplication, updateApplicationStatusWise } from '../controllers/application.controller.js';
-
+import { protect } from '../middleware/auth.middleware.js';
 const router =  express.Router();
 
 
-router.post('/', createApplication);
-router.get('/', getApplications);
-router.put('/:id',updateApplication)
-router.patch('/:id/status',updateApplicationStatusWise)
-router.delete('/:id',deleteApplication)
+router.post('/', protect, createApplication);                    // Create new application
+router.get('/', protect, getApplications);                       // Get all user's applications
+router.put('/:id', protect, updateApplication);                  // Update application
+router.patch('/:id/status', protect, updateApplicationStatusWise); // Update status only
+router.delete('/:id', protect, deleteApplication);               // Delete application
 
 export default router;
