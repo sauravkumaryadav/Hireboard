2f9330412714cddbba6e2256126ca9bf6986df1b
Saurav Kumar Yadav <sauravskyjiit@gmail.com>
2026-05-06 16:20:43 +0530

    application controllers added auth

--- server/src/routes/application.routes.js
+++ server/src/routes/application.routes.js
@@ -1,16 +1,24 @@
-// Application Routes
-
 import express from 'express';
-import Application from '../models/Application.js';
-import { createApplication, deleteApplication, getApplications, updateApplication, updateApplicationStatusWise } from '../controllers/application.controller.js';
+import {
+  createApplication,
+  deleteApplication,
+  getApplications,
+  updateApplication,
+  updateApplicationStatusWise,
+  getApplicationById
+} from '../controllers/application.controller.js';
+
 import { protect } from '../middleware/auth.middleware.js';
-const router =  express.Router();
 
+const router = express.Router();
 
-router.post('/', protect, createApplication);                    // Create new application
-router.get('/', protect, getApplications);                       // Get all user's applications
-router.put('/:id', protect, updateApplication);                  // Update application
-router.patch('/:id/status', protect, updateApplicationStatusWise); // Update status only
-router.delete('/:id', protect, deleteApplication);               // Delete application
+router.post('/', protect, createApplication);
+router.get('/', protect, getApplications);
+
+router.patch('/:id/status', protect, updateApplicationStatusWise);
+
+router.get('/:id', protect, getApplicationById);
+router.put('/:id', protect, updateApplication);
+router.delete('/:id', protect, deleteApplication);
 
 export default router;
